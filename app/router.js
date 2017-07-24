cf455e80280a46568131c2f91312eed815ba261e
Victor Ngeno <ngenovictor321@gmail.com>
2017-07-24 19:04:02 +0300

    update the admin page

--- app/router.js
+++ app/router.js
@@ -7,8 +7,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('admin');
-  this.route('fooditem',{path:'admin/fooditem/:fooditem'});
+  this.route('admin',{path:'/admin'});
+  this.route('fooditem',{path:'/admin/fooditem/:fooditem'});
 });
 
 export default Router;
