d51a668453eb60fa223c8cd7b32e688fc69ef05b
Victor Ngeno <ngenovictor321@gmail.com>
2017-07-24 14:41:57 +0300

    update model with removing of plurals

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model(){
+		return this.store.findAll('fooditem');
+	},
 	actions:{
 		submitNewItem(params){
 			var newFoodItem = this.store.createRecord('fooditem', params);
