a1961c54a42207cbf083e5eec89932f831204435
derrickwaweru <derrickwaweru38@gmail.com>
2017-07-25 14:12:39 +0300

    added the select order function

--- app/routes/kitchen.js
+++ app/routes/kitchen.js
@@ -1,15 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model (params){
-    this.store.query("order",{
+  model (){
+    return this.store.query("order",{
       orderBy:"processed_status",
-      equalTo:false
+      equalTo:"pending"
     })
   },
   actions: {
-    destroyOrder(order) {
-      order.destroyOrder();
+    pendingOrder(order) {
+      order.pendingOrder();
+    },
+    update(order, params){
+      Object.keys(params).forEach(function (key) {
+        if (params[key]!==undefined) {
+          order.set(key,params[key]);
+        }
+      });
+      order.save();
     }
    }
 });
