237833738d25558395495548409becca2a2de428
mosoti17 <mosoti17@gmail.com>
2017-07-25 15:22:58 +0300

    added pending and cooking orders

--- app/routes/kitchen.js
+++ app/routes/kitchen.js
@@ -1,11 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model (){
-    return this.store.query("order",{
-      orderBy:"processed_status",
-      equalTo:"pending"
+
+  model() {
+    return Ember.RSVP.hash({
+      pending:this.store.query('order', {
+        orderBy: 'processed_status',
+        equalTo: 'pending',
+
+      }),
+      cooking:this.store.query('order', {
+        orderBy: 'processed_status',
+        equalTo: 'cooking',
+
+      }),
+
     })
+
   },
   actions: {
     pendingOrder(order) {
