035703c431f29a469831c2a54cc494adfb98e75d
Victor Ngeno <ngenovictor321@gmail.com>
2017-07-25 15:24:44 +0300

    homepage can now display all categories of food items

--- app/components/view-order.js
+++ app/components/view-order.js
@@ -16,13 +16,15 @@ export default Ember.Component.extend({
       var params = {
         fooditems: this.get('orderBasket.items'),
         date_created:"today",
-        processed_status:false,
+        processed_status:"pending",
         total_price: this.get('totalPrice'),
         table_number:this.get('table'),
         customer_id:this.get('customer_id'),
         tray_number:0,
       };
-      this.sendAction('confirmOrder',params);
+      if(confirm("Are you sure you want to place that order?")){
+        this.sendAction('confirmOrder',params);
+      };
     }
   }
   
