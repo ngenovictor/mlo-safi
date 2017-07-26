771d7cce23f3e9337cb86ff1db97a37b4677d7eb
Victor Ngeno <ngenovictor321@gmail.com>
2017-07-26 14:55:40 +0300

    update models

--- app/models/order.js
+++ app/models/order.js
@@ -1,12 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-    fooditems: DS.hasMany('orderitem', {async:true}),
+    fooditems: DS.hasMany('fooditem',{async:true}),
     date_created: DS.attr(),
     processed_status: DS.attr(),
     total_price: DS.attr(),
     table_number: DS.attr(),
-    customer: DS.belongsTo('user'),
+    customer_id: DS.attr(),
     tray_number: DS.attr(),
-    // staffactions: DS.hasMany('staffaction', {async:true})
 });
